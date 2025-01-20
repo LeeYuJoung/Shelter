9dbda0c42102ce3b474f60e3f1d280789a816810
LeeYuJoung <nania000822@gmail.com>
2025-01-20 23:40:08 +0900

    Update Robot

---
+++ Assets/02.Scripts/Robot/RobotController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+using EnumTypes;
+
+namespace yjlee.Robot
+{
+    public class RobotController : MonoBehaviour
+    {
+        private RobotType robotType;
+        public float moveSpeed;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
